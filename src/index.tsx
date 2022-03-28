46bde7ff56fc64ade7d490b72ab05b197e1561ff
Oleg Vashchan <guker168@gmail.com>
2022-03-28 16:16:36 +0300

    Add esBuild config simple transpilation

--- src/index.tsx
+++ src/index.tsx
@@ -1,17 +1,50 @@
-import React from 'react';
+import * as esbuild from 'esbuild-wasm';
+import { useState, useEffect, useRef } from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+
+const App = () => {
+  const [input, setInput] = useState('');
+  const [code, setCode] = useState('');
+  const codeRef = useRef(false);
+
+  const startService = async () => {
+    await esbuild.initialize({
+      worker: true,
+      wasmURL: '/esbuild.wasm'
+    });
+    codeRef.current = true;
+  };
+
+  const submitCodeHanlder = async () => {
+    if (!codeRef.current) {
+      return;
+    }
+    const result = await esbuild.transform(input, {
+      loader: 'jsx',
+      target: 'es2015',
+    })
+    setCode(result.code);
+  };
+
+  const inputHandler: React.ChangeEventHandler<HTMLTextAreaElement> = (event) => {
+    setInput(event.target.value);
+  };
+
+  useEffect(() => {
+    startService();
+  }, []);
+
+  return (
+    <div>
+      <textarea value={input} onChange={inputHandler}></textarea>
+      <div>
+        <button onClick={submitCodeHanlder}>Submit</button>
+      </div>
+      <pre>{code}</pre>
+    </div>
+  )
+}
+
+ReactDOM.render(<App />, document.querySelector('#root'))
