26f805a871992c9f90c31803270dc38e18369a5e
Oleg Vashchan <guker168@gmail.com>
2022-03-28 21:33:21 +0300

    Add imports config & esbuild path plugin & axios

--- src/index.tsx
+++ src/index.tsx
@@ -1,13 +1,13 @@
 import * as esbuild from 'esbuild-wasm';
 import { useState, useEffect, useRef } from 'react';
 import ReactDOM from 'react-dom';
-
+import { unpkgPathPlugin } from './plugins/unpkg-path-plugin';
 
 
 const App = () => {
   const [input, setInput] = useState('');
-  const [code, setCode] = useState('');
-  const codeRef = useRef(false);
+  const [code, setCode] = useState<any>();
+  const codeRef: any = useRef();
 
   const startService = async () => {
     await esbuild.initialize({
@@ -15,17 +15,26 @@ const App = () => {
       wasmURL: '/esbuild.wasm'
     });
     codeRef.current = true;
+    console.log('esbuild init!')
   };
 
   const submitCodeHanlder = async () => {
     if (!codeRef.current) {
       return;
     }
-    const result = await esbuild.transform(input, {
-      loader: 'jsx',
-      target: 'es2015',
+    // const result = await esbuild.transform(input, {
+    //   loader: 'jsx',
+    //   target: 'es2015',
+    // })
+
+    const result = await esbuild.build({
+      entryPoints: ['index.js'],
+      bundle: true,
+      write: false,
+      plugins: [unpkgPathPlugin()]
     })
-    setCode(result.code);
+    console.log(result.outputFiles[0].text);
+    setCode(result.outputFiles[0].text);
   };
 
   const inputHandler: React.ChangeEventHandler<HTMLTextAreaElement> = (event) => {
